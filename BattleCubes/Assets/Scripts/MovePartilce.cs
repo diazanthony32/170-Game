db914ef02e315c3f831fe4e69bc9cde695952b8d
Alejandro Landaverde <adlandav@ucsc.edu>
2020-05-20 23:35:20 -0700

    changed the sprite on the simple slash attack

---
+++ BattleCubes/Assets/Scripts/MovePartilce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePartilce : MonoBehaviour
+{
+    bool movingPositive = false;
+    float angle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.rotateAround(gameObject, Vector3.forward, 360, 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}

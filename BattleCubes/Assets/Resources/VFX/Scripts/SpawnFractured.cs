a06e1a1023ff826cfacd5bde3aac1d6292bf12e1
Alejandro Landaverde <adlandav@ucsc.edu>
2020-05-28 14:12:43 -0700

    unity is bullshit

---
+++ BattleCubes/Assets/Resources/VFX/Scripts/SpawnFractured.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFractured : MonoBehaviour
+{
+    public GameObject original;
+    public GameObject fractured;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            SpawnFracturedObj();
+        }
+    }
+
+    public void SpawnFracturedObj() {
+        //Destroy(original);
+        DestroyImmediate(original, true);
+        GameObject fractObj = Instantiate(fractured) as GameObject;
+        fractObj.transform.GetChild(1).GetComponent<explode>().ExplodeObject();
+    }
+}

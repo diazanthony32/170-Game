aab953276dbd8f55e506b280240bae49fcc217a5
Alejandro Landaverde <adlandav@ucsc.edu>
2020-05-27 22:46:38 -0700

    changed the song

---
+++ BattleCubes/Assets/Resources/VFX/Scripts/explode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour
+{
+    public float destroyDelay;
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    public void Start() {
+        //StartCoroutine(ExplodeObject());
+        ExplodeObject();
+    }
+
+    public void ExplodeObject() {
+
+        //yield return new WaitForSeconds(5);
+
+        foreach (Transform t in transform) {
+            var rb = t.GetComponent<Rigidbody>();
+
+            if (rb != null) {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+
+            Destroy(t.gameObject, destroyDelay);
+        } 
+    }
+}

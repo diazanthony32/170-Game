b3369118e857fcc7e5a4151ec6f3f246ed3f53c8
Alejandro Landaverde <adlandav@ucsc.edu>
2020-05-29 20:44:47 -0700

    Added sams desintegration effect

--- BattleCubes/Assets/Resources/VFX/Scripts/SpawnFractured.cs
+++ BattleCubes/Assets/Resources/VFX/Scripts/SpawnFractured.cs
@@ -8,29 +8,42 @@ public class SpawnFractured : MonoBehaviour
     public Vector3 explosionOffset;
     public GameObject original;
     public GameObject fractured;
+    public GameObject hellCube;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float val = 0.0f;
+    bool start = false;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
             SpawnFracturedObj();
+            StartCoroutine(Disolve());
+        }
+
+        if (start && val < 1.0f) {
+            val += 0.005f;
+
+            for (int i = 6; i < 12; i++) {
+                hellCube.transform.GetChild(i).GetComponent<MeshRenderer>().materials[0].SetFloat("_Fade", val);
+                hellCube.transform.GetChild(i).GetComponent<MeshRenderer>().materials[1].SetFloat("_Fade", val);
+            }
         }
     }
 
+    IEnumerator Disolve() {
+        yield return new WaitForSeconds(4);
+        start = true;
+    }
+
     public void SpawnFracturedObj() {
+
         if (explosion != null) {
             GameObject explosionFX = Instantiate(explosion) as GameObject;
-            Destroy(explosionFX, 3);
+            Destroy(explosionFX, 10);
         }
 
-        Destroy(original);
-        GameObject fractObj = Instantiate(fractured) as GameObject;
-        fractObj.transform.GetChild(1).GetComponent<explode>().ExplodeObject();
+        //Destroy(original);
+        //GameObject fractObj = Instantiate(fractured) as GameObject;
+        //fractObj.transform.GetChild(1).GetComponent<explode>().ExplodeObject();
     }
 }

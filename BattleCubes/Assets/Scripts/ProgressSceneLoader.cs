8fcbd7b7efd2013cdb78db1dfe9cf6c6ed3eeedd
Alejandro Landaverde <adlandav@ucsc.edu>
2020-09-11 15:42:10 -0700

    progress scene loader update
    progress scene loader now doesn't create multiple copies of itself, and it works for both server and client. There's still a minor error with unknown cause.

--- BattleCubes/Assets/Scripts/ProgressSceneLoader.cs
+++ BattleCubes/Assets/Scripts/ProgressSceneLoader.cs
@@ -12,12 +12,20 @@ public class ProgressSceneLoader : MonoBehaviour
     [SerializeField] TextMeshProUGUI ProgressText;
     [SerializeField] Slider ProgressBar;
 
+    private static bool ProgressSceneLoaderExists;
+
     private AsyncOperation operation;
     private Canvas canvas;
 
     public void Awake() {
-        canvas = GetComponentInChildren<Canvas>(true);
-        DontDestroyOnLoad(gameObject);
+        if (!ProgressSceneLoaderExists) {
+            ProgressSceneLoaderExists = true;
+            canvas = GetComponentInChildren<Canvas>(true);
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
     }
 
     public void Loadscene(string sceneName = "") {
@@ -52,6 +60,7 @@ public class ProgressSceneLoader : MonoBehaviour
 
         while (!operation.isDone) {
             UpdateProgressUI(operation.progress);
+            //print(operation);
             yield return null;
         }
 
